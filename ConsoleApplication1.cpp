bb496f9949e21c4ff6335cc085748205fc8aa929
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-07-11 09:57:04 +0700

    output ke layar

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -5,4 +5,12 @@ int n;
 
 void input() {
 
+	while (true) {
+		cout << "Masukkan banyaknya elemen pada array : ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\rArray dapat mempunyai maksimal 20 elemen.\n";              //output ke layar
+		}
 	
