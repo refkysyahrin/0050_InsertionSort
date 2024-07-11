c56c129d8c3ed7d8e658f545cb8be101dae261b3
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-07-11 09:59:27 +0700

    step 2

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -26,9 +26,11 @@ void input() {
 	}
 }
 
-void insertionSort() {
+	void insertionSort() {
 	int i, temp, j;
 
 	for (i = 1; i <= n - 1; i++) {         //step 1
+
+		temp = arr[i];                     //step 2
 
 		
